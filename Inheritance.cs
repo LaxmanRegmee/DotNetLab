74f4ac3ee231fba614ea419b6c6cf1c0dd12a39b
Laxman Regmi <laxmanregmee7@gmail.com>
2024-11-30 11:45:01 +0545

    first commit

---
+++ Inheritance.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpLxmn
+{
+    internal class Employee
+    {
+        public double salary = 40000;
+    }
+    class Programmer : Employee
+    {
+        public double bonus = 5000;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpLxmn
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+           Programmer obj = new Programmer();
+           Console.WriteLine(obj.salary);
+           Console.WriteLine(obj.bonus);
+
+
+          Employee emp = new Employee();
+          Console.WriteLine(emp.salary) ; 
+
+         }
+        
+    }
+}
